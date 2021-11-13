900556fb491dda7870cfd1d8dbd56be886934adb
ayanomorita <s24al139@seinan-gu.ac.jp>
2021-11-13 07:03:31 +0000

    first submit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd80ab9393a1c20c2fe52eeb5c946a320931db4cbaa87cb8bb0ebcb6cda5552ed51de946270e84f6ca295b0027d1336b34434945614562d89e2539d07ce1bfae3'
